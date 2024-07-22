c46d129e5a5b8e46e7bfb4e2baab7a8af2961ac7
Yutaro Ohno <yutaro.ono.418@gmail.com>
2024-07-22 21:28:54 +0900

    Parse URL with scheme, host, path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #[derive(Debug)]
 struct URL {
     scheme: String,
+    host: String,
+    path: String,
 }
 
 impl URL {
@@ -10,12 +12,20 @@ impl URL {
             return Err("Excpected an URL that has a scheme".into());
         }
 
-        let (scheme, _rest) = (
+        let (scheme, rest) = (
             std::mem::replace(&mut scheme_and_rest[0], "".into()),
             std::mem::replace(&mut scheme_and_rest[1], "".into()),
         );
 
-        Ok(URL { scheme })
+        let mut host_and_rest: Vec<String> = rest.split("/").map(|s| s.into()).collect();
+        if host_and_rest.len() < 1 {
+            return Err("Excpected an URL that has a scheme".into());
+        }
+
+        let host = std::mem::replace(&mut host_and_rest[0], "".into());
+        let path = host_and_rest.join("/");
+
+        Ok(URL { scheme, host, path })
     }
 }
 
@@ -25,18 +35,24 @@ mod tests {
 
     #[test]
     fn new_succeed() {
+        let url = URL::new("http://example.com/path/to/somewhere.html").unwrap();
+        assert_eq!(url.scheme, "http");
+        assert_eq!(url.host, "example.com");
+        assert_eq!(url.path, "/path/to/somewhere.html");
+
         let url = URL::new("http://example.com").unwrap();
         assert_eq!(url.scheme, "http");
-
-        let url = URL::new("https://example.com").unwrap();
-        assert_eq!(url.scheme, "https");
+        assert_eq!(url.host, "example.com");
+        assert_eq!(url.path, "");
     }
 
     #[test]
     fn new_fail() {
+        // No scheme.
         let url = URL::new("example.com");
         assert!(url.is_err());
 
+        // Multiple schemes.
         let url = URL::new("http://http://example.com");
         assert!(url.is_err());
     }
